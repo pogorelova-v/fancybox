4d06c6be855f97d733da78761ebb01eb46ee8959
Pogorelova <gorelikovavn@gmail.com>
2023-03-16 10:59:41 +0300

    ff

---
+++ script.js
@@ -0,0 +1,19 @@
+Fancybox.show([{ src:"#modal1", type: "inline" }]);
+
+Fancybox.bind("[data-fancybox='btn_modal2']", {
+    // Your custom options
+  });
+Fancybox.bind("[data-fancybox='btn_modal3']", {
+    // Your custom options
+  });
+Fancybox.bind("[data-fancybox='btn_modal4']", {
+    // Your custom options
+  });
+Fancybox.bind("[data-fancybox='btn_modal5']", {
+  });
+Fancybox.bind("[data-fancybox='btn_modal6']", {
+    // Your custom options
+  });
+Fancybox.bind("[data-fancybox='btn_modal7']", {
+    // Your custom options
+  });
